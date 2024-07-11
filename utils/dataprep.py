f1341e3dece0389d258350a39a4586c28e4e3a14
alebelluco <148531503+alebelluco@users.noreply.github.com>
2024-07-11 12:58:06 +0200

    Add files via upload

--- utils/dataprep.py
+++ utils/dataprep.py
@@ -1,12 +1,11 @@
 # raggruppamento utility
 # 26-06-2024
 
-
 import pandas as pd 
 import streamlit as st 
 from io import BytesIO
 import xlsxwriter
-
+import zipfile
 
 def unisci_colonne(df, colonne, new):
     new_col = [] 
@@ -26,7 +25,6 @@ def unisci_colonne(df, colonne, new):
 
     return df
 
-
 def crea_chiave(df, dic_key):
     df['key']=None
     for i in range(len(df)):
@@ -38,12 +36,12 @@ def crea_chiave(df, dic_key):
         df['key'].iloc[i] = ''.join((key))
     return df
 
-
 def scarica_excel(df, filename):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
     df.to_excel(writer, sheet_name='Sheet1',index=False)
     writer.close()
+
     st.download_button(
         label="Download Excel workbook",
         data=output.getvalue(),
@@ -51,7 +49,47 @@ def scarica_excel(df, filename):
         mime="application/vnd.ms-excel"
     )
 
+def create_zip(df_dict):
+    zip_buffer = BytesIO()
+    with zipfile.ZipFile(zip_buffer, "a") as zip_file:
+        for fornitore in df_dict.keys():
+            data = df_dict[fornitore]
+            excel_frame = BytesIO()
+            
+            file_name = f'{fornitore}.xlsx'
+            writer = pd.ExcelWriter(excel_frame, engine='xlsxwriter')
+            data.to_excel(writer,sheet_name='Sheet1',index=False)
+            zip_file.writestr(file_name,data.to_excel(writer,sheet_name='Sheet1',index=False))
+            writer.close()
+            
+
+            st.write(zip_buffer.getvalue())         
+
+    st.download_button(
+        label="Download Excel workbook",
+        data=zip_buffer.getvalue(),
+        file_name='Sottoscorta.zip',
+        mime="application/zip"
+    )
+    
+def multi(df):
+    with pd.ExcelWriter('out.xlsx', engine='xlsxwriter') as writer:
+        df.to_excel(writer, sheet_name="stocks")
 
 
 
+def create_excel_file(df, file_name):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='openpyxl')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    writer.close()
+    processed_data = output.getvalue()
+    return processed_data
+
+def create_zip_file(files):
+    zip_buffer = BytesIO()
+    with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED, False) as zip_file:
+        for file_name, data in files.items():
+            zip_file.writestr(file_name, data)
+    return zip_buffer.getvalue()
 
