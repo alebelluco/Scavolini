0a4fdc774e01c8503620681a6a2638c4f1c6d5e5
alebelluco <148531503+alebelluco@users.noreply.github.com>
2025-05-09 15:46:07 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,6 +4,6 @@ import pandas as pd
 
 st.set_page_config(layout='wide')
 
-st.image('https://github.com/alebelluco/Scavolini/blob/main/Scav.png?raw=True')
-
+#st.image('https://github.com/alebelluco/Scavolini/blob/main/Scav.png?raw=True')
+st.image('foto.JPG')
 st.divider()
